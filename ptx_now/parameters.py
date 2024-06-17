152b3a6eab081d7c2f9aaaf20fe98f00e411c6eb
ulicious <uwe.langenmayr@kit.edu>
2024-06-17 12:24:20 +0200

    Include missing demand

---
+++ ptx_now/parameters.py
@@ -0,0 +1,17 @@
+costs_missing = 3.75
+cost_selling = 3.25
+
+electricity_available = True
+electricity_price = 10
+
+demand_type = 'total'
+
+country = 'Germany'
+countries = ['Germany', 'Australia', 'Kazakhstan', 'Saudi Arabia', 'Chile']
+
+# Chile, australia
+
+framework_name = 'FT_all_data_no_scaling.yaml'
+
+cluster_length = 336
+cluster_lengths = [168, 336]
